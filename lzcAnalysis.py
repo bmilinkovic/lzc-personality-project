6bffa6e1bf3ff4a64a019ab52988cd88f66c764c
borjan milinkovic <borjan.milinkovic@gmail.com>
2022-08-31 12:56:15 +1000

    "created for loop to loop over all participants"

--- lzcAnalysis.py
+++ lzcAnalysis.py
@@ -1,29 +1,25 @@
+# importing all the modules we need
+
 from src import lzc
 import mne
 import os
+from pathlib import Path
 
-
-# Get the data for 1 participant. Here ideally, you would get the data of all participants, store them somehow
-# for later analysis pipeline
+# Loop over all participants in data/ directory in project.
+# The loop also calculates the total LZc for each participant's multidimensional time-series and saves in the variable
+# lz_all
 dataDir = "data/"
-data = mne.io.read_raw_bdf(os.path.join(dataDir, "P1.bdf"))
+ext = ".bdf"
+lz_all = []
+for filename in os.listdir(dataDir):
+    if filename.endswith(ext):
+        f = os.path.join(dataDir, filename)
+        data = mne.io.read_raw_bdf(f)
+        raw_array = data[:][0]
+        LZc_single_subject = lzc.LZc(raw_array)
+        lz_all.append(LZc_single_subject)
 
-# To extract JUST the raw data file as a numpy array, or 'matrix' we need to select this from the raw data structure
-# that MNE creates, so lets get that:
 
-raw_array = data[:][0]
 
-# Now begin Lempel-Ziv Analysis:
-# As above, here you would ideally create a for loop that will loop over every subject.
-# I actually think that all of it can be put in a for loop,
-# i.e.
-#   for each file in the data folder:
-#           load that file
-#           extract the raw numpy array 'matrix' for Lzc computation
-#           compute LZc on that multidimensional array
-#           save the lempelziv complexity into a variable and keep appending this variable with new quantities.
-#           so then we have a vector valued variable of all lempel-ziv complexities for each participant.
-#   end for loop.
 
-lzc.LZc(raw_array)
 
