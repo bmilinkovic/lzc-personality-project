6a0113c1dffcb5a7dc4e8b8f3737f850228d2d6d
borjan milinkovic <borjan.milinkovic@gmail.com>
2022-09-01 20:35:25 +1000

    "created seperate scripts for loading in .edf and .csv files respectively. "

---
+++ lzcAnalysis_csv.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import os
+import mne
+from src import lzc
+
+
+# This file will load all files in the data directory ending with the .csv
+# extension and same the final values in a vector called lzc_all_csv.
+dataDir = "data/"
+ext_csv = ".csv"
+lz_all_csv = []
+for filename in os.listdir(dataDir):
+    if filename.endswith(ext_csv):
+        f = os.path.join(dataDir, filename)
+        file_csv = pd.read_csv(f).to_numpy().T
+        LZc_single_subject_csv = lzc.LZc(raw_array)
+        lz_all_csv.append(LZc_single_subject_csv)
---
+++ lzcAnalysis_edf.py
@@ -0,0 +1,35 @@
+import numpy as np
+from src import lzc
+import mne
+import os
+
+
+# This file will load all files in the data directory ending with the .csv
+# extension and same the final values in a vector called lzc_all_csv.
+
+dataDir = "data/"
+ext_edf = ".edf"
+lz_all_edf = []
+
+for filename in os.listdir(dataDir):
+    if filename.endswith(ext_edf):
+        f = os.path.join(dataDir, filename)
+        data = mne.io.read_raw_edf(f)
+        raw_array = data[:][0]
+        LZc_single_subject_edf = lzc.LZc(raw_array)
+        lz_all_edf.append(LZc_single_subject_edf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
